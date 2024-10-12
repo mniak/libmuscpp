42f432e75877f79ab683d8436f27a0c5f6ccf67e
Mniak <me@mniak.dev>
2024-10-12 18:44:15 -0300

    example/c working

--- examples/c/main.c
+++ examples/c/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <libmus.h>
 
-void main() {
+int main() {
     int a = 3;
     int b = 5;
     int c = add(a, b);
     printf("Math: %d + %d = %d\n", a, b, c);
+    return 0;
 }
--- src/libmus.zig
+++ src/libmus.zig
@@ -1,3 +1,3 @@
-export fn test_2(a: i32, b: i32) i32 {
+export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
